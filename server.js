45137a6050594e8dbea8ebaefa960a054365c81f
avinash <avinash.kumar>
2018-09-26 13:26:55 +0530

    added partials and maintenance

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000; //to configure Heroku Port
-
 var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   //fs.appendFile('server.log', log + '\n');
+   next();
+
+});
+
+// app.use((req,res,next)=>{
+//    res.render('maintenance.hbs');
+// });
+
+
 app.use(express.static(__dirname+'/public'));
 
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+})
+
 app.get('/', (req, res)=>{
-    //res.send('<h1>hello express</h1>');
-    res.send({
-      name:'avinash',
-      likes:['reading','travelling']
-    })
+    res.render('home.hbs');
 });
 
 app.get('/about',(req,res)=>{
